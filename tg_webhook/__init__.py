7cfa90720fce8573ab4fc74385bb82f6d54adeb7
Matheus Marchini <matheus@sthima.com.br>
2015-10-19 19:35:42 -0200

    Some validations and log messages

--- tg_webhook/__init__.py
+++ tg_webhook/__init__.py
@@ -1,6 +1,9 @@
 
+import logging
+
 import requests
 
+log = logging.getLogger(__name__)
 
 triggers = []
 
@@ -9,17 +12,19 @@ class WebHook(object):
 
     _registered_hooks = {}
 
-    def __init__(self, hook, method, auth, callback=None):
+    def __init__(self, hook, url, method, auth, execute, callback=None):
         self.hook = hook
+        self.url = url
         self.method = method
-        self.auth = auth
+        self.authenticate = auth
+        self.execute = execute
         self.callback = callback
 
     @classmethod
-    def register_hook(cls, hook, url, method, auth, callback=None):
+    def register_hook(cls, hook, url, method, auth, execute, callback=None):
         if not cls._registered_hooks.get(hook):
             cls._registered_hooks[hook] = []
-        web_hook = WebHook(hook, url, method, auth, callback)
+        web_hook = WebHook(hook, url, method, auth, execute, callback)
         cls._registered_hooks[hook].append(web_hook)
 
     @classmethod
@@ -28,13 +33,21 @@ class WebHook(object):
 
     @classmethod
     def trigger_hook(cls, hook, kwargs):
-        for hook in cls._registered_hooks[hook]:
+        for hook in cls._registered_hooks.get(hook, []):
             request = dict(
                 url=hook.url,
                 method=hook.method
             )
-            hook.authenticate(request, kwargs)
-            hook.execute(request, kwargs)
+            request = hook.authenticate(request, kwargs)
+            if not request:
+                continue
+            request = hook.execute(request, kwargs)
+            if not request:
+                continue
+            url = request.get("url")
+            method = request.get("method")
+            log.debug("Making request [%s %s]" % (method, url))
             response = requests.request(**request)
+            log.debug("Status code to request [%s %s]: %s" % (method, url, response.status_code))
             if hook.callback:
                 hook.callback(response, kwargs)
